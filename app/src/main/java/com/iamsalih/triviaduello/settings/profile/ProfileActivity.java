31b32b28adca7969d70200cba8f7cb959d56958d
Muhammed Salih Guler <gueler@karlmax-berlin.com>
2017-12-12 10:56:37 +0100

    TD-32: Add image adding to server.

--- app/src/main/java/com/iamsalih/triviaduello/settings/profile/ProfileActivity.java
+++ app/src/main/java/com/iamsalih/triviaduello/settings/profile/ProfileActivity.java
@@ -1,15 +1,24 @@
 package com.iamsalih.triviaduello.settings.profile;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.iamsalih.triviaduello.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by muhammedsalihguler on 09.12.17.
@@ -17,9 +26,54 @@ import com.iamsalih.triviaduello.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private static final int RC_PHOTO_PICKER = 1;
+    @BindView(R.id.profile_picture)
+    ImageView profilePicture;
+
+    @BindView(R.id.delete_account_button)
+    Button deleteAccount;
+
+    private StorageReference mChatPhotosStorageReference;
+    private FirebaseStorage mFirebaseStorage;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_fragment_view);
+        ButterKnife.bind(this);
+        mFirebaseStorage = FirebaseStorage.getInstance();
+        mChatPhotosStorageReference = mFirebaseStorage.getReference().child("chat_photos");
+
+    }
+
+    @OnClick(R.id.profile_picture)
+    public void addProfileImage() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/jpeg");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_PHOTO_PICKER && resultCode == RESULT_OK) {
+            Uri selectedImageUri = data.getData();
+
+            // Get a reference to store file at chat_photos/<FILENAME>
+            StorageReference photoRef = mChatPhotosStorageReference.child(selectedImageUri.getLastPathSegment());
+
+            // Upload file to Firebase Storage
+            photoRef.putFile(selectedImageUri)
+                    .addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // When the image has successfully uploaded, we get its download URL
+                            Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                            Glide.with(ProfileActivity.this).load(downloadUrl).into(profilePicture);
+                        }
+                    });
+        }
     }
 }
